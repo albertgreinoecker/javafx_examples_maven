6e2dd13f961e8443ea6f43c43697aa1bec506729
albert <albert.greinoecker@gmail.com>
2025-04-22 09:22:16 +0200

    correct naming in module-info

--- fx_examples_maven/src/main/java/module-info.java
+++ fx_examples_maven/src/main/java/module-info.java
@@ -18,8 +18,8 @@ module fx_examples_maven.fx_examples_maven {
     exports fx_examples_maven.fx_examples_maven.ex_12_tables;
     exports fx_examples_maven.fx_examples_maven.ex_13_images;
     exports fx_examples_maven.fx_examples_maven.ex_14_images_background;
-    exports fx_examples_maven.fx_examples_maven.ex_15_canvas_first;
-    exports fx_examples_maven.fx_examples_maven.ex_16_canvas_group;
+    exports fx_examples_maven.fx_examples_maven.ex_15_canvas_groups;
+    exports fx_examples_maven.fx_examples_maven.ex_16_canvas_first;
     exports fx_examples_maven.fx_examples_maven.ex_17_menu;
     exports fx_examples_maven.fx_examples_maven.ex_18_web_view;
     exports fx_examples_maven.fx_examples_maven.ex_19_TTT;
